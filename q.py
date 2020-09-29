fe89545cb38eb225a7bfd50c2726c47630003f0c
popov-dmitriy <71076068+popov-dmitriy@users.noreply.github.com>
2020-09-29 10:22:25 +0300

    Delete q.py

--- q.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-con = sqlite3.connect("database.db")
-link = con.cursor()
-
-link.execute("SELECT name, category, price FROM game")
-con.commit()
-print(link.fetchall())
-link.fetchall()
